bcb1bdda156d6ce2dd35be5f2860bebf79e33660
EC2 Default User <111130918+roherstein@users.noreply.github.com>
2022-10-06 15:05:43 +0000

    [finish]4-17

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,23 +1,2 @@
 class UserController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    @post_images = @User.post_images
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
   
-  def update
-    @user = User.find(params[:id])
-    @user.update(User_params)
-    redirect_to post_image_path(user.id)
-  end
-  
-  private
-  
-  def user_params
-    params.require(:user).permit(:name,:profile_image)
-  end
-  
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :post_images, only: [:new,:create,:index,:show,:destroy]
-  resources :users,only: [:show,:edit,:update]
+  resources :users,only: [:show,:edit,:update,:index]
   get 'homes/about' => 'homes#about', as:'about'
 end
