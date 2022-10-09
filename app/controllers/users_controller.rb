a03d30612819e9694a5c6cdabe44eb24cd8244fb
EC2 Default User <111130918+roherstein@users.noreply.github.com>
2022-10-09 03:12:49 +0000

    [finish]4-19

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to post_image_path(user.id)
+    redirect_to post_image_path(@user.id)
   end
 
   private
