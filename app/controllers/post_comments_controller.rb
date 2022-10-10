25524e6ffe81b6df3a3aef6d7944017822b9afc7
EC2 Default User <111130918+roherstein@users.noreply.github.com>
2022-10-10 11:19:26 +0000

    [finish]4-23

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,6 +1,7 @@
 class PostCommentsController < ApplicationController
+
   def create
-    post_image = PostImage.find(params[post_image_id])
+    post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_image_id = post_image.id
     comment.save
@@ -11,11 +12,11 @@ class PostCommentsController < ApplicationController
     PostComment.find(params[:id]).destroy
     redirect_to post_image_path(params[:post_image_id])
   end
- 
+
   private
 
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
-  
+
 end
